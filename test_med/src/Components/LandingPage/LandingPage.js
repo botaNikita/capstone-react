9f55d8ac459af9548eae7f5f3324c5997c98c7d3
botaNikita <nikita.a.timofeev@gmail.com>
2024-07-07 23:53:39 +0200

    Navbar and Landing page are on React

--- test_med/src/Components/LandingPage/LandingPage.js
+++ test_med/src/Components/LandingPage/LandingPage.js
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">  
-    <link rel="stylesheet" href="./LandingPage.css">
-</head>
-<body>
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
     <section class="hero-section">
         <div>
           <div data-aos="fade-up" class="flex-hero">
@@ -39,5 +32,7 @@
   
         </div>
       </section>
-</body>
-</html>
+  );
+};
+
+export default LandingPage; 
