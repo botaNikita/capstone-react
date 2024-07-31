a6f75f49adb5907d96f1c3933489cd320b718d65
botaNikita <nikita.a.timofeev@gmail.com>
2024-07-31 22:42:13 +0200

    Find Doctor Search component

--- test_med/src/App.js
+++ test_med/src/App.js
@@ -10,6 +10,7 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 // Function component for the main App
 function App() {
@@ -28,6 +29,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
             <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/search/doctors" element={<FindDoctorSearch />} />
           </Routes>
         </BrowserRouter>
     </div>
